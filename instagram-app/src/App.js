e07964ffe547835c124daad6d9df588352d032c8
Jeremy Morales <jeremy.morales@gmail.com>
2019-06-02 23:02:28 -0700

    changed icons

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+
+        {/* <SearchBar />
+        <PostContainer /> */}
+      </div>
+    
+    );
+  }
+  
 }
+
 
 export default App;
---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './SearchBar.css';
+import iglogo from '../assets/ig_search_bar_logo.png';
+import compass from './assets/compass.png';
+import heart from './assets/heart.png';
+import user from './assets/user.png';
+import { Form, Input } from 'reactstrap';
+
+const SearchBar = props => {
+    return (
+      <div className="search-bar">
+        <div className="logo-wrapper">
+          <img className="ig-logo" src={iglogo} alt="" />
+        </div>
+        <div className="search-form">
+          <Form>
+            <Input
+              type="search"
+              name="search"
+              id="searchText"
+              placeholder="search"
+            />
+          </Form>
+        </div>
+        <div className="socials-wrapper">
+            <img className="socials-img" src={compass} alt='' />
+            <img className="socials-img" src={heart} alt='' />
+            <img className="socials-img" src={user} alt='' />
+        </div>
+      </div>
+    );
+}
+
+export default SearchBar;
